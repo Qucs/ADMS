c4d6265fa86281b25a2028af7f13da60143dcfd9
Luther T. Cat <lutherthecat@github>
2014-01-12 23:08:49 -0600

    Use create_files.sh to generate files for CMake build system.
    * Use static config.h.
    
    * use proper bison option as opposed to moving files from y.tab.c
    
    * use bison -Dapi.prefix when version >= 2.6

---
+++ admsXml/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H_FILE
+#define CONFIG_H_FILE
+#define HAVE_STDLIB_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STRING_H 1
+#define PACKAGE_NAME "adms"
+#define PACKAGE_VERSION "2.3.0"
+#define PACKAGE_BUGREPORT "lutherthecat@github"
+#endif
