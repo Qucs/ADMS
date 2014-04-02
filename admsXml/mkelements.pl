30ae22549eaf87f45179041d75fb81bb6eeff356
Guilherme Brondani Torri <guitorri@gmail.com>
2014-04-02 23:35:44 +0200

    fix api.prefix, tested bison 2.7.1 and 3.0.2
    * Enable build with both, Autotools and CMake
    
    * Note that bison versions handle api.prefix diferently:
      bison (GNU Bison) 2.7.1 fail with    : %define api.prefix {..}
      bison (GNU Bison) 3.0.2 warning with : %define api.prefix "..."
    
    * Settle with "..." for the moment
      CMake solution to pass "-Dapi.prefix=" is fine, but how to achieve that with Automake?

--- admsXml/mkelements.pl
+++ admsXml/mkelements.pl
@@ -1951,7 +1951,7 @@ print "$top_srcdir/admsXml/admstpathYacc.h: created\n";
 
 open admstpathYacc_y,">$top_srcdir/admsXml/admstpathYacc.y";
 print admstpathYacc_y "$BUILDER
-%define api.prefix {admstpath}
+%define api.prefix \"admstpath\"
 %lex-param {p_pparse mypparse}
 %parse-param {p_pparse mypparse}
 \%{
