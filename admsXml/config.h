ee6fb3460910062ba79bcb4c6fa22fcc6c7b443a
Guilherme Brondani Torri <guitorri@gmail.com>
2014-05-14 15:49:40 +0200

    release 2.3.2
    * bump version
    * add qucs-devel list for bugs

--- admsXml/config.h
+++ admsXml/config.h
@@ -4,6 +4,6 @@
 #define HAVE_SYS_STAT_H 1
 #define HAVE_STRING_H 1
 #define PACKAGE_NAME "adms"
-#define PACKAGE_VERSION "2.3.0"
+#define PACKAGE_VERSION "2.3.2"
 #define PACKAGE_BUGREPORT "lutherthecat@github"
 #endif
