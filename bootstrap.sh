cc5af8dc033fa4938f6bd429174a58513dceb4fc
Guilherme Brondani Torri <guitorri@gmail.com>
2014-05-19 13:11:43 +0200

    fix bootstrap script
    * cd into dir to run scripts
    * run libtool to fix issue:
      configure.ac:7: error: required directory ./build-aux does not exist

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 # !/bin/sh
 
+here=`pwd`
+cd `dirname $0`
+
 echo "bootstrapping the ADMS sources..."
 echo "Creating aclocal.m4... "
 aclocal -I m4
@@ -7,6 +10,12 @@ echo "Creating headers... "
 autoheader
 echo "Creating configure... "
 autoconf
+echo "Libtoolizing... "
+case `uname` in
+  *Darwin*) LIBTOOLIZE=glibtoolize ;;
+  *)        LIBTOOLIZE=libtoolize ;;
+esac
+$LIBTOOLIZE
 echo "Creating Makefiles... "
 automake --copy --add-missing --foreign
 echo "done."
