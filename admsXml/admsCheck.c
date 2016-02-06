f6153d2cb501c12f280091b1ebcd0f904092f6e9
Guilherme Brondani Torri <guitorri@gmail.com>
2016-02-06 16:25:11 +0100

    license: add new header to *.c

--- admsXml/admsCheck.c
+++ admsXml/admsCheck.c
@@ -1,4 +1,26 @@
 /*
+This file is part of adms - http://sourceforge.net/projects/mot-adms.
+
+adms is a code generator for the Verilog-AMS language.
+
+Copyright (C) 2002-2012 Laurent Lemaitre <r29173@users.sourceforge.net>
+              2013 Richard Crozier <richard.crozier@example.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  * RCS Info
  * $Id: admsCheck.c 1110 2008-10-14 13:40:29Z r29173 $
  *
